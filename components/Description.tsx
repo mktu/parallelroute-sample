f11cb333d95bd488669b9ecab74be54c06dead2c
mktu <mushi9ui@gmail.com>
2024-09-02 18:25:47 +0900

    fix description

--- components/Description.tsx
+++ components/Description.tsx
@@ -5,8 +5,8 @@ const Description: FC = () => {
         <section className='p-4'>
             <h3 className='font-semibold text-lg'>Parallel route checker</h3>
             <p className='text-sm mt-2'>
-                "Links"からルートを選択すると、選択されたルートに対応したコンポーネントが"Components"セクションにレンダリングされます。
-                <br />また、その時レンダリングされたファイルパスが"List"セクションにハイライトされます。
+                Linksからルートを選択すると、選択されたルートに対応したコンポーネントがComponentsセクションにレンダリングされます。
+                <br />また、その時レンダリングされたファイルパスがListセクションにハイライトされます。
             </p>
         </section>
     );
